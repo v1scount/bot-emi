8c0ab102e7f58ce9e939db74aed10d47027528bf
Rodrigo Vellido <rodrigovellido1@gmail.com>
2022-10-22 03:12:05 -0300

    fix deploy on render

--- index.js
+++ index.js
@@ -5,22 +5,12 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express()
-const port = 8080;
+const port = process.env.PORT || 3030;
 
 app.listen(port, () => {
 
     const { Client, LocalAuth } = require('whatsapp-web.js');
-    const client = new Client(
-        {
-            puppeteer: {
-                args: [
-                    '--no-sandbox',
-                    '--disable-setuid-sandbox',
-                    '--user-data-dir=' + dataDir
-                ]
-            }
-        }
-    );
+    const client = new Client();
     
     
     
